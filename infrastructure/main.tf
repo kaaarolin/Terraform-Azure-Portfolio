a09b9082698162f080bceb16bb252d5541558736
Karolin <karolin_57@hotmail.com>
2026-05-04 14:46:11 +0200

    added workspace

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -44,11 +44,22 @@ resource "azurerm_linux_web_app" "app" {
   }
 }
 
+resource "azurerm_log_analytics_workspace" "workspace" {
+  name = "portfolio-workspace" 
+  location = var.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  sku = "PerGB2018"
+  retention_in_days = 30 
+  tags = var.tags
+}
+
 resource "azurerm_application_insights" "app-insights" {
   name = "portfolio-app-insights" 
   location = var.location
   resource_group_name = data.azurerm_resource_group.rg.name
+  workspace_id = azurerm_log_analytics_workspace.workspace.id
   application_type = "web" 
+  tags = var.tags
 }
 
 output "instrumentation_key" {
@@ -56,6 +67,6 @@ output "instrumentation_key" {
   # sensitive = true 
 }
 
-output "instrumentation_key" {
+output "app_id" {
   value = azurerm_application_insights.app-insights.app_id
 }
